5bc0cdf47c43935e8dab5dd965060a7c8790eb72
racstan <asthanarachit@gmail.com>
2024-05-12 21:57:03 +0530

    Reversion

--- interpreterMain.js
+++ interpreterMain.js
@@ -1,80 +1,32 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.interpretExpression = exports.interpret = void 0;
+exports.interpret = void 0;
 var operatorUtils_1 = require("./operatorUtils");
-var functions = {};
-function handleStatement(statement, variables, output) {
-    switch (statement.type) {
-        case 'assignment':
-            if (!statement.variable || !statement.expression)
-                throw new Error('Invalid assignment statement');
-            variables[statement.variable] = interpretExpression(statement.expression, variables);
-            break;
-        case 'output':
-            if (!statement.expression)
-                throw new Error('Invalid output statement');
-            output.push(interpretExpression(statement.expression, variables));
-            break;
-        case 'conditional':
-            if (!statement.condition || !statement.trueBranch)
-                throw new Error('Invalid conditional statement');
-            if (interpretExpression(statement.condition, variables) !== 0) {
-                output.push.apply(output, interpret(statement.trueBranch, variables, output)); // Fixed: Added missing arguments
-            }
-            else if (statement.falseBranch) {
-                output.push.apply(output, interpret(statement.falseBranch, variables, output)); // Fixed: Added missing arguments
-            }
-            break;
-        default:
-            throw new Error("Unknown statement type: ".concat(statement.type));
-    }
-}
-function interpret(ast, variables, output) {
-    // Removed the initialization of variables and output here to use the passed parameters instead
-    if (variables === void 0) { variables = {}; }
-    if (output === void 0) { output = []; }
+function interpret(ast) {
+    var output = [];
+    var variables = {};
     for (var _i = 0, ast_1 = ast; _i < ast_1.length; _i++) {
         var statement = ast_1[_i];
         try {
             switch (statement.type) {
-                case 'comment':
-                case 'multiline_comment':
+                case 'assignment':
+                    if (!statement.variable || !statement.expression)
+                        throw new Error('Invalid assignment statement');
+                    variables[statement.variable] = interpretExpression(statement.expression, variables);
                     break;
-                case 'function':
-                    if ('name' in statement && 'params' in statement && 'body' in statement) {
-                        functions[statement.name] = statement;
-                    }
-                    else {
-                        throw new Error('Invalid function definition');
-                    }
+                case 'output':
+                    if (!statement.expression)
+                        throw new Error('Invalid output statement');
+                    output.push(interpretExpression(statement.expression, variables));
                     break;
-                case 'call':
-                    if ('name' in statement && 'args' in statement) {
-                        var func = functions[statement.name];
-                        if (!func)
-                            throw new Error("Function ".concat(statement.name, " is not defined"));
-                        if (func.params.length !== statement.args.length)
-                            throw new Error("Function ".concat(statement.name, " expects ").concat(func.params.length, " arguments but got ").concat(statement.args.length));
-                        var oldVariables = __assign({}, variables);
-                        for (var i = 0; i < func.params.length; i++) {
-                            variables[func.params[i]] = interpretExpression(statement.args[i], variables);
-                        }
-                        var result = interpret(func.body, variables, output); // Fixed: Added missing arguments
-                        variables = oldVariables;
+                case 'conditional':
+                    if (!statement.condition || !statement.trueBranch)
+                        throw new Error('Invalid conditional statement');
+                    if (interpretExpression(statement.condition, variables) !== 0) {
+                        output.push.apply(output, interpret(statement.trueBranch));
                     }
-                    else {
-                        throw new Error('Invalid function call');
+                    else if (statement.falseBranch) {
+                        output.push.apply(output, interpret(statement.falseBranch));
                     }
                     break;
                 case 'block':
@@ -82,11 +34,34 @@ function interpret(ast, variables, output) {
                         throw new Error('Invalid block statement');
                     for (var _a = 0, _b = statement.statements; _a < _b.length; _a++) {
                         var innerStatement = _b[_a];
-                        handleStatement(innerStatement, variables, output);
+                        switch (innerStatement.type) {
+                            case 'assignment':
+                                if (!innerStatement.variable || !innerStatement.expression)
+                                    throw new Error('Invalid assignment statement');
+                                variables[innerStatement.variable] = interpretExpression(innerStatement.expression, variables);
+                                break;
+                            case 'output':
+                                if (!innerStatement.expression)
+                                    throw new Error('Invalid output statement');
+                                output.push(interpretExpression(innerStatement.expression, variables));
+                                break;
+                            case 'conditional':
+                                if (!innerStatement.condition || !innerStatement.trueBranch)
+                                    throw new Error('Invalid conditional statement');
+                                if (interpretExpression(innerStatement.condition, variables) !== 0) {
+                                    output.push.apply(output, interpret(innerStatement.trueBranch));
+                                }
+                                else if (innerStatement.falseBranch) {
+                                    output.push.apply(output, interpret(innerStatement.falseBranch));
+                                }
+                                break;
+                            default:
+                                throw new Error("Unknown inner statement type: ".concat(innerStatement.type));
+                        }
                     }
                     break;
                 default:
-                    handleStatement(statement, variables, output);
+                    throw new Error("Unknown statement type: ".concat(statement.type));
             }
         }
         catch (error) {
@@ -117,9 +92,6 @@ function interpretExpression(expression, variables) {
     for (var _i = 0, expression_1 = expression; _i < expression_1.length; _i++) {
         var token = expression_1[_i];
         switch (token.type) {
-            case 'comment':
-            case 'multiline_comment':
-                break;
             case 'number':
                 stack.push(Number(token.value));
                 break;
@@ -136,7 +108,7 @@ function interpretExpression(expression, variables) {
             case 'arithmetic_operator':
             case 'operator':
                 while (stack.length > 1 && precedence[stack[stack.length - 2]] >= precedence[token.value]) {
-                    (0, operatorUtils_1.applyOperatorToStack)(stack, functions, variables);
+                    (0, operatorUtils_1.applyOperatorToStack)(stack);
                 }
                 stack.push(token.value);
                 break;
@@ -145,8 +117,7 @@ function interpretExpression(expression, variables) {
         }
     }
     while (stack.length > 1) {
-        (0, operatorUtils_1.applyOperatorToStack)(stack, functions, variables);
+        (0, operatorUtils_1.applyOperatorToStack)(stack);
     }
     return stack[0];
 }
-exports.interpretExpression = interpretExpression;
--- operatorUtils.js
+++ operatorUtils.js
@@ -1,54 +1,17 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.applyOperator = exports.applyOperatorToStack = void 0;
-var interpreterMain_1 = require("./interpreterMain");
-function applyOperatorToStack(stack, functions, variables) {
+function applyOperatorToStack(stack) {
     var right = stack.pop();
     var operator = stack.pop();
     var left = stack.pop();
-    if (!['+', '-', '*', '/', '%', '**', '>', '<', '==', '!=', '>=', '<=', '&&', '||'].includes(operator)) {
+    if (typeof operator !== 'string' || !['+', '-', '*', '/', '%', '**', '>', '<', '==', '!=', '>=', '<=', '&&', '||'].includes(operator)) {
         throw new Error("Operator ".concat(operator, " is not supported"));
     }
-    else if (operator in functions) {
-        var func = functions[operator];
-        if (!func)
-            throw new Error("Function ".concat(operator, " is not defined"));
-        if (func.params.length !== 2)
-            throw new Error("Function ".concat(operator, " expects 2 arguments but got ").concat(func.params.length));
-        var oldVariables_1 = __assign({}, variables);
-        variables[func.params[0]] = left;
-        variables[func.params[1]] = right;
-        var result = (0, interpreterMain_1.interpret)(func.body, variables);
-        Object.keys(variables).forEach(function (key) {
-            if (oldVariables_1.hasOwnProperty(key)) {
-                variables[key] = oldVariables_1[key];
-            }
-            else {
-                delete variables[key];
-            }
-        });
-        stack.push(result);
-    }
-    else {
-        stack.push(applyOperator(operator, left, right));
-    }
+    stack.push(applyOperator(operator, left, right));
 }
 exports.applyOperatorToStack = applyOperatorToStack;
 function applyOperator(operator, operand1, operand2) {
-    if (typeof operand1 !== 'number' || typeof operand2 !== 'number') {
-        throw new Error("Operands must be numbers");
-    }
     switch (operator) {
         case '+':
             return operand1 + operand2;
